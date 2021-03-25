1b87f4c973611a16d8eadd36bffeb6eb4a6b7825
Manoela <manoela.pala@gmail.com>
2021-03-25 19:17:39 -0300

    primeiro commit

--- src/main/java/com/danilo/tdd_bdd/validadorCpf/controllers/HomeController.java
+++ src/main/java/com/danilo/tdd_bdd/validadorCpf/controllers/HomeController.java
@@ -1,7 +1,10 @@
 package com.danilo.tdd_bdd.validadorCpf.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.danilo.tdd_bdd.validadorCpf.models.Cliente;
 
 @Controller
 public class HomeController {
@@ -9,5 +12,13 @@ public class HomeController {
   @GetMapping("/")
   public String index(){
     return "home/index";
+  }
+
+
+ @GetMapping("/valida-cpf")
+  public String validaCPF(Cliente cliente, Model model){
+    boolean verdade = cliente.validarCPF();
+    model.addAttribute("verdade", verdade);
+    return "home/cpfValidado";
   }
 }
--- src/test/java/com/danilo/tdd_bdd/validadorCpf/ValidadorCpfApplicationTests.java
+++ src/test/java/com/danilo/tdd_bdd/validadorCpf/ValidadorCpfApplicationTests.java
@@ -1,13 +1,62 @@
 package com.danilo.tdd_bdd.validadorCpf;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.danilo.tdd_bdd.validadorCpf.models.Cliente;
 
 @SpringBootTest
 class ValidadorCpfApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("dada que o campo teste seja digitado corretamente com um cpf válido")
+	void fazendoTesteDeCpfValido() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Manoela");
+		cliente.setCpf("09533107448");
+		assertEquals(true, cliente.validarCPF());
+	}
+
+	@Test
+	void fazendoTesteDeCpfInvalido() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Manoela");
+		cliente.setCpf("095331074");
+		assertEquals(false, cliente.validarCPF());
+	}
+
+	@Test
+	void fazendoTesteDeCpfValidoComPontos() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Manoela");
+		cliente.setCpf("095.331.074-48");
+		assertEquals(true, cliente.validarCPF());
+	}
+
+	@Test
+	void fazendoTesteDeCpfInvalidoComPontos() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Manoela");
+		cliente.setCpf("095.331.000-00");
+		assertEquals(false, cliente.validarCPF());
+	}
+
+	@Test
+	void fazendoTesteDeCpfInvalidoVazio() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Manoela");
+		cliente.setCpf("");
+		assertEquals(false, cliente.validarCPF());
+	}
+
+	@Test
+	void fazendoTesteDeCpfInvalidoComLetras() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Manoela");
+		cliente.setCpf("letras");
+		assertEquals(false, cliente.validarCPF());
 	}
 
 }
